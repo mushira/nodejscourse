a382d310a022ed40e1bcef525dbc0ef3a1e538b5
Wojtek Maku <eryniel93@gmail.com>
2017-02-21 18:57:45 +0000

    injections

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,23 @@ gulp.task('style', function(){
 });
 
 gulp.task('inject', function(){
+    var wiredep = require("wiredep").stream;
+    var inject = require("gulp-inject");
     
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    
+    var injectOptions = {
+        ignorePath: '/public'
+    }
+    
+    var options = {
+        bowerJson: require("./bower.json"),
+        directory: './bower_components',
+        ignorePath: '../../bower_components'
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views/'));
 });
